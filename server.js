b5cbef8a4a3f89a05a9108dc2a5be99f3c7e016b
Aryan Suri <aryansuri@campus-101-157.ucdavis.edu>
2021-09-16 18:22:58 -0700

    alpha0.0.1

---
+++ server.js
@@ -0,0 +1,23 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express();
+const expressLayout = require('express-ejs-layouts')
+const indexRouter = require('./routes/index')
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayout)
+app.use(express.static('public'))
+
+mongoose.connect(process.env.DATABASE_URL, {
+});
+const db = mongoose.connection
+db.on('error', function (err) {console.error(err)});
+db.once('open', function () {console.log('connection open')});
+
+app.use('/', indexRouter)
+app.listen(process.env.PORT || 3000)
