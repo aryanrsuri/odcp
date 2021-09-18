de20334cecdfc149bc683a93b3d1246190d7e9df
Aryan Suri <arsuri@ucdavis.edu>
2021-09-18 13:23:36 -0700

    alpha0.1.3-bugindev

--- models/sample.js
+++ models/sample.js
@@ -1,14 +1,27 @@
 const mongoose = require('mongoose')
-
+const sample_file_path = 'uploads/files'
+const sample_image_path = 'uploads/images'
 const sampleSchema = new mongoose.Schema({
     sample_name: {
         type: String,
         required: true
     },
     sample_description: {
-        type: Buffer,
-        required: false
+        type: String,
+        required: true
+    },
+    sample_image: {
+        type: String,
+        required: true
     }
 })
+// sampleSchema.virtual('sample_file_path').get(function() {
+//     if(this.sample_description != null){
+         
+//     }
+// });
 
-module.exports = mongoose.model("Sample", sampleSchema)
+
+module.exports = mongoose.model("Sample", sampleSchema);
+module.exports.sample_file_path = sample_file_path;
+module.exports.sample_image_path = sample_image_path;
--- routes/samples.js
+++ routes/samples.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const Sample = require('../models/sample');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const upload_path = path.join('public', Sample.sample_file_path)
+const image_upload_path = path.join('public', Sample.sample_image_path)
 const router = express.Router();
+const upload_types = ['text/csv','text/plain', 'application/msword']
+const image_upload_types = ['image/jpeg', 'image/jpg', 'image/png']
+const upload = multer({
+    dest: upload_path,
+    fileFilter: (req, file, callback) => {
+        callback(null, upload_types.includes(file.mimetype))
+    }
+})
+
+const imageUpload = multer({
+    dest: image_upload_path,
+    fileFilter: (req, file, callback) => {
+        callback(null, image_upload_types.includes(file.mimetype))
+    }
+})
+
 // All samples route
 router.get('/',  async (req, res) => {
     let searchOptions = {}
@@ -25,16 +46,19 @@ router.get('/new', (req, res) => {
 })
 
 // Create sample Router
-router.post('/', async (req, res) => {
+router.post('/', fileUpload, async (req, res) => {
+    const file_name = req.file != null ? req.file.filename : null
     const sample = new Sample({
         sample_name: req.body.compound,
-        sample_description: req.body.upload
+        sample_description: file_name,
+        sample_image: file_name
     })
     try {
         const newSample = await sample.save()
         //res.redirect(`samples/$newSample.id}`)
         res.redirect('samples')
     } catch {
+        if (sample.sample_description != null) {remove_file(sample.sample_description) }
         res.render('samples/new', {
             sample: sample,
             errorMessage: 'Error creating Author'
@@ -42,4 +66,15 @@ router.post('/', async (req, res) => {
     }
 })
 
+function fileUpload(req, res, next) {
+    upload.single('upload')(req, res, next);
+    imageUpload.single('imgUpload')(req, res, next);
+    next();
+}
+
+function remove_file(fileName){
+    fs.unlink(path.join(upload_path, fileName), error => {
+        if(error) console.error(error)
+    })
+}
 module.exports = router;
